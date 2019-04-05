6e2ff391c4ead9bd754edd3553bfbe95f49425fd
sjenterrement <sjenterrement@gmail.com>
2019-04-05 10:15:25 +0800

    1004..submited

--- 1004/Counting Leaves.cpp
+++ 1004/Counting Leaves.cpp
@@ -1,3 +1,7 @@
+/*
+给出父子节点关系，求每层叶子个数
+此题规定根节点为01，故不用创建node结构和寻找根节点
+*/
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -5,13 +9,13 @@ using namespace std;
 vector<int> v[100];
 int book[100], maxdepth = -1;
 void dfs(int index, int depth) {
-	if (v[index].size() == 0) {
-		book[depth]++;
-		maxdepth = max(maxdepth, depth);
+	if (v[index].size() == 0) {//当节点为叶子结点时，给此节点确定层数
+		book[depth]++;//每层叶子节点个数
+		maxdepth = max(maxdepth, depth);//计算层数
 		return;
 	}
 	for (int i = 0; i < v[index].size(); i++)
-		dfs(v[index][i], depth + 1);
+		dfs(v[index][i], depth + 1);//深度优先，遍历每条路径，孩子节点深度＋1
 }
 int main() {
 	int n, m, k, node, c;
@@ -23,7 +27,7 @@ int main() {
 			v[node].push_back(c);
 		}
 	}
-	dfs(1, 0);
+	dfs(1, 0);//设置root节点为1
 	printf("%d", book[0]);
 	for (int i = 1; i <= maxdepth; i++)
 		printf(" %d", book[i]);
